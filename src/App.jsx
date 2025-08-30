547b8f0fc5f786ffb1ad6edffc19ff3fae2b1061
romerocruzsa <sebastian.cruz6@upr.edu>
2025-08-30 05:30:01 -0400

    feat: update app structure and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
 import Footer from "./components/Footer";
@@ -20,11 +16,7 @@ export default function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
             <Route path="/products" element={<Products />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="/privacy" element={<Privacy />} />
           </Routes>
         </main>
